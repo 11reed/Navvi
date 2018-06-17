740934393d36e8cd645b66a8bb511733976a00eb
Tim <timreed27@yahoo.com>
2018-06-17 04:21:49 -0400

    Update soundplayer.js

--- app/sound/soundplayer.js
+++ app/sound/soundplayer.js
@@ -20,7 +20,7 @@ export default class Sound extends Component<Props> {
 
     render() {
       return (
-      <View style={styles.menu}>
+      <View>
       <Image source onPress={{uri: 'https://i.imgur.com/FrCYqvS.png'}}
         style={{width: 15, height: 15, alignSelf: 'center', float: left}} />
       </View>
